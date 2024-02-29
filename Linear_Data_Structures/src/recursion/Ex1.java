eebb7afa7996023049584151aa4f34efa5ab0f33
AkashShetty2001 <akashkd9911@gmail.com>
2024-02-29 15:02:24 +0530

    recursion concept

---
+++ Linear_Data_Structures/src/recursion/Ex1.java
@@ -0,0 +1,8 @@
+package recursion;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
---
+++ Linear_Data_Structures/src/recursion/Questions/Problem1.java
@@ -0,0 +1,20 @@
+package recursion.Questions;
+
+public class Problem1 {
+	public static void main(String[] args) {
+		System.out.println("recursion");
+		print(10);
+	}
+	public static void print(int n) {
+		
+		//bc
+		if(n ==1) {
+			System.out.println(n);
+			return;
+		}
+		
+		System.out.print(n+" "); //work
+		print(n-1);// calling inner function
+	}
+
+}
