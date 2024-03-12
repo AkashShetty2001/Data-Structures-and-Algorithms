afa3b68c4fd778e409137b86fbb9be99e6cf4cf4
AkashShetty2001 <akashkd9911@gmail.com>
2024-03-12 21:58:44 +0530

    basic problem to understand Recursion

---
+++ Linear_Data_Structures/src/recursion/Questions/Problem2.java
@@ -0,0 +1,25 @@
+package recursion.Questions;
+
+/*
+ * increasing Order
+ */
+
+public class Problem2 {
+	public static void main(String[] args) {
+		//System.out.println("recursion");
+		print(15);
+	}
+	public static void print(int n) {
+		
+		//bc
+		if(n ==1) {
+			System.out.print(n+" ");
+			return;
+		}
+		
+		
+		print(n-1);// calling inner function
+		System.out.print(n+" "); //work
+	}
+
+}
