0c79f2552b1c30d7513c0bb45dac09b97b7a3c74
AkashShetty2001 <akashkd9911@gmail.com>
2024-03-13 17:24:25 +0530

    Added comments

--- Linear_Data_Structures/src/recursion/Ex1.java
+++ Linear_Data_Structures/src/recursion/Ex1.java
@@ -1,8 +1,20 @@
 package recursion;
-
+/*
+ * Recursion is a technique of making a function call itself.
+ * 
+ */
 public class Ex1 {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		//System.out.println("hello");
+		print(5);
+	}
+	
+	public static void print(int n) {
+		if(n < 0) {
+			return;
+		}
+		System.out.println("Hello World!");
+		print(n-1);
 	}
 
 }
