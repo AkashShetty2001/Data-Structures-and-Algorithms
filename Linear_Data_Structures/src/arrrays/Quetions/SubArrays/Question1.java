d4256fac8a3e62677792a6987a004fc9b8fb85b9
AkashShetty2001 <akashkd9911@gmail.com>
2024-06-27 21:55:57 +0530

    Subarray questions

---
+++ Linear_Data_Structures/src/arrrays/Quetions/SubArrays/Question1.java
@@ -0,0 +1,34 @@
+package arrrays.Quetions.SubArrays;
+import java.util.HashSet;
+
+/*
+ * return true if a Subarray == k (given sum); 
+ */
+public class Question1 {
+	public static void main(String[] args) {
+		int a[]= {2,8,2,6,-6,3,2};
+		int k=12;
+		System.out.println(subarray1(a,k));
+	}
+	
+	public static boolean subarray1(int a[],int k) {
+		HashSet<Integer> hs = new HashSet<>();
+		
+		//to handle  sum=k
+		hs.add(0);
+		
+		int sum =0;
+		for(int i=0;i<a.length;i++) {
+			sum += a[i];
+			int rem = sum-k;
+			
+			if(hs.contains(rem)) {
+				return true;
+			}
+			
+			hs.add(sum);
+		}
+		return false;
+	}
+
+}
