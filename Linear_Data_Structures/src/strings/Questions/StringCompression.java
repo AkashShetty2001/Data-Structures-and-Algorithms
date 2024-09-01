a3c066e3174dc266707879079d0e766222975a98
AkashShetty2001 <akashkd9911@gmail.com>
2024-09-01 08:26:57 +0530

    added comments

--- Linear_Data_Structures/src/strings/Questions/StringCompression.java
+++ Linear_Data_Structures/src/strings/Questions/StringCompression.java
@@ -1,5 +1,34 @@
 package strings.Questions;
 
 public class StringCompression {
+	public static void main(String[] args) {
+		
+		String s= "aaaaaabbbbcccdddddd";
+		System.out.println(compress(s));
+		
+
+	
+	}
+	
+	public static String compress(String s) {
+		
+		String newstr="";
+		for(int i=0;i<s.length();i++) {
+			Integer count=1;
+			
+			while(i<s.length()-1 && s.charAt(i) == s.charAt(i+1)){
+				count++;
+				i++;
+			}
+			
+			newstr= newstr+s.charAt(i);
+			
+			if(count > 1) {
+				newstr= newstr+count.toString();
+			}
+		}
+		return newstr;
+		
+	}
 
 }
