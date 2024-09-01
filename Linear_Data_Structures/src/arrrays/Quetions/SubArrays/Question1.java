f8f771a2362cf124ff698d95cf3e7dcc93ba54dc
AkashShetty2001 <akashkd9911@gmail.com>
2024-09-01 08:26:04 +0530

    added comments

--- Linear_Data_Structures/src/arrrays/Quetions/SubArrays/Question1.java
+++ Linear_Data_Structures/src/arrrays/Quetions/SubArrays/Question1.java
@@ -2,33 +2,32 @@ package arrrays.Quetions.SubArrays;
 import java.util.HashSet;
 
 /*
- * return true if a Subarray == k (given sum); 
+ *  pattern1 return true if a Subarray == k (given sum); 
  */
 public class Question1 {
 	public static void main(String[] args) {
 		int a[]= {2,8,2,6,-6,3,2};
-		int k=12;
+		int k=5
+				;
 		System.out.println(subarray1(a,k));
 	}
 	
 	public static boolean subarray1(int a[],int k) {
-		HashSet<Integer> hs = new HashSet<>();
 		
-		//to handle  sum=k
+		HashSet<Integer> hs = new HashSet<>();
 		hs.add(0);
 		
-		int sum =0;
+		int sum=0;
 		for(int i=0;i<a.length;i++) {
-			sum += a[i];
-			int rem = sum-k;
-			
+			sum+=a[i];
+			int rem=sum-k;
 			if(hs.contains(rem)) {
 				return true;
 			}
-			
 			hs.add(sum);
 		}
 		return false;
+		
 	}
 
 }
