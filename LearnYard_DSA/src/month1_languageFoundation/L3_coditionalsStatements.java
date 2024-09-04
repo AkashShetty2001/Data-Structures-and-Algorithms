6ef1541c9c271f4a87909f9410663998ff75831c
AkashShetty2001 <akashkd9911@gmail.com>
2024-09-04 22:15:27 +0530

    codes on conditional statemnet concept

---
+++ LearnYard_DSA/src/month1_languageFoundation/L3_coditionalsStatements.java
@@ -0,0 +1,49 @@
+package month1_languageFoundation;
+
+public class L3_coditionalsStatements {
+	public static void main(String[] args) {
+		
+		//
+		/*
+		 * 1. if(condition){
+		 *       statement // if condition true
+		 *       };
+		 */
+		/*
+		int marks =39;
+		if(marks >= 39) {
+			System.out.println("congrats!! you passed..");
+			
+		}
+		*/
+		
+		/*
+		 * 2. if else
+		 * if(condition){
+		 * // statement // true
+		 * }else{
+		 *  // statements // condition false
+		 *  }
+		 */
+		
+//		int marks =40;
+//		if(marks >= 39) {
+//			System.out.println("congrats!! you passed..");	
+//		}else {
+//			System.out.println("Hard luck you failed");
+//		}
+		
+		//nested if else
+		int a=10;
+		if(a>=10) {
+			if(a==10) {
+				System.out.println("the value is 10");
+			}else {
+				System.out.println("value is greater then 10");
+			}
+		
+		}
+		
+	}
+
+}
