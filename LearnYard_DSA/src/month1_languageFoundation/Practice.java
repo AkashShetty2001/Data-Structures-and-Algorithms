3a9836ea1b498fe3a3468be1c1c123d4be9a4671
AkashShetty2001 <akashkd9911@gmail.com>
2024-09-04 22:16:17 +0530

    practice snippers

---
+++ LearnYard_DSA/src/month1_languageFoundation/Practice.java
@@ -0,0 +1,22 @@
+package month1_languageFoundation;
+import java.util.Scanner;
+
+public class Practice {
+	public static void main(String args[]) {
+		
+		
+		System.out.println("Hello");
+		Scanner in = new Scanner(System.in);
+//		int i=in.nextInt();
+//		double d=in.nextDouble();
+//		float f=in.nextFloat();
+//		char c=in.next().charAt(0);
+		
+		int num1=5;
+		double num2=5.0;
+		boolean b=(num1 == num2);
+		System.out.println(b);
+		
+	}
+
+}
