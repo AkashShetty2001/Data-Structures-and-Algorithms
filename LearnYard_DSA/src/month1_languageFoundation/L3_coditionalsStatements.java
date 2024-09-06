839d769337b8c0e01cefcfe2b88986dc133043c3
AkashShetty2001 <akashkd9911@gmail.com>
2024-09-06 21:00:01 +0530

    added more basic Questions

--- LearnYard_DSA/src/month1_languageFoundation/L3_coditionalsStatements.java
+++ LearnYard_DSA/src/month1_languageFoundation/L3_coditionalsStatements.java
@@ -1,7 +1,9 @@
 package month1_languageFoundation;
+import java.util.Scanner;
 
 public class L3_coditionalsStatements {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		//
 		/*
@@ -34,14 +36,35 @@ public class L3_coditionalsStatements {
 //		}
 		
 		//nested if else
-		int a=10;
-		if(a>=10) {
-			if(a==10) {
-				System.out.println("the value is 10");
-			}else {
-				System.out.println("value is greater then 10");
-			}
+//		int a=10;
+//		if(a>=10) {
+//			if(a==10) {
+//				System.out.println("the value is 10");
+//			}else {
+//				System.out.println("value is greater then 10");
+//			}
+//		
+//		}
 		
+		// print days of week using a given day no through if else
+		System.out.println("Please enter the day no, 1 means monday and 7 means sunday...");
+		int dayNo=sc.nextInt();
+		if(dayNo == 1) {
+			System.out.println("Monday");
+		}else if(dayNo == 2) {
+			System.out.println("Tuesday");
+		}else if(dayNo == 3) {
+			System.out.println("wednesday");
+		}else if(dayNo == 4) {
+			System.out.println("Thrusday");
+		}else if(dayNo == 5) {
+			System.out.println("Friday");
+		}else if(dayNo == 6) {
+			System.out.println("Saturday");
+		}else if(dayNo == 7) {
+			System.out.println("Sunday");
+		}else {
+			System.out.println("Invalid day Number....");
 		}
 		
 	}
