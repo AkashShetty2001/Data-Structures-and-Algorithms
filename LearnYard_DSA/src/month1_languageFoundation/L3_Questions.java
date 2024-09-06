f8d7a11612028f37cae08b7e728e89f37c19f22c
AkashShetty2001 <akashkd9911@gmail.com>
2024-09-06 21:00:45 +0530

    basic Question on switch, if else concept

---
+++ LearnYard_DSA/src/month1_languageFoundation/L3_Questions.java
@@ -0,0 +1,91 @@
+package month1_languageFoundation;
+import java.util.Scanner;
+
+public class L3_Questions {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		
+//		//max of 2
+//		
+//		System.out.println("Enter 2 numbers to check max.");
+//		int a=sc.nextInt();
+//		int b=sc.nextInt();
+//		
+//		if(a>b) {
+//			System.out.println("number "+ a+" is greater");
+//		}else if(a==b) {
+//			System.out.println("number "+a+" and  "+b+" are equal ");
+//		}else {
+//			System.out.println("number "+b+" is greater");
+//		}
+		
+		//max of 3 using logical operators
+		/*
+		 *  1. && Logical and operator.(both condition should be true)
+		 *  2. || Logical OR operator (any one cond, shpuld be true)
+		 *  3, !  Logica=l Not operator ( reverse)
+		 */
+//		
+//		int a=111;
+//		int b=111;
+//		int c=12;
+//		
+//		if(a>= b && a>=c) {
+//			System.out.println("a is greater");
+//		}else if(b>=c && b>=a) {
+//			System.out.println("b is greater");
+//		}else {
+//			System.out.println("c is greater");
+//		}
+//		
+		
+		//Ternary operator
+//		// (condition)? true: false;
+//		int max = (9> 8) ? 9 : 8;
+//		System.out.println(max);
+		
+		
+		
+		//switch case..
+		//days of a week using 
+//		System.out.println("Enter day No..");
+//		 int dayNo=sc.nextInt();
+//		 
+//		 switch(dayNo) {
+//		 case 1:
+//			 System.out.println("Monday");
+//			 break;
+//			 
+//		 case 2:
+//			 System.out.println("Tuesday");
+//			 break;
+//			 
+//		 case 3:
+//			 System.out.println("Wednesday");
+//			 break;
+//			 
+//		 case 4:
+//			 System.out.println("Thrusday");
+//			 break;
+//			 
+//		 case 5:
+//			 System.out.println("Friday");
+//			 break;
+//			 
+//		 case 6:
+//			 System.out.println("saturday");
+//			 break;
+//			 
+//		 case 7:
+//			 System.out.println("Sunday");
+//			 break;
+//			 
+//		default:
+//			System.out.println("Invalid day No");
+//				 
+//			 
+//		 }
+	}
+
+}
