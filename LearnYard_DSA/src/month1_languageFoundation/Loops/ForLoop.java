c7e93e9e0b91e40ea5a8d7d8636e764c42433ffd
AkashShetty2001 <akashkd9911@gmail.com>
2024-09-07 22:25:27 +0530

    code snippet to understand for loop

--- LearnYard_DSA/src/month1_languageFoundation/Loops/ForLoop.java
+++ LearnYard_DSA/src/month1_languageFoundation/Loops/ForLoop.java
@@ -1,5 +1,21 @@
 package month1_languageFoundation.Loops;
 
 public class ForLoop {
+	public static void main(String[] args) {
+		 /*
+		  * for loop
+		  * use for loop when you have to move from one place to another place.
+		  * 
+		  *syntax:
+		  *for(initialization (1); test condition (2);update expression(4)){
+		  *	     // statements(3)
+		  *   };
+		  */
+		
+		for(int i=1;i<=3;i=i+1) {
+			System.out.println("value of i is "+i);
+			System.out.println("Hello");
+		}
+	}
 
 }
