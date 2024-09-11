b658c214404f846b7311d10c6d2cc24e47aa0c31
AkashShetty2001 <akashkd9911@gmail.com>
2024-09-11 23:29:52 +0530

    added comments

--- LearnYard_DSA/src/month1_languageFoundation/Typecasting/ImplicitTypeCasting.java
+++ LearnYard_DSA/src/month1_languageFoundation/Typecasting/ImplicitTypeCasting.java
@@ -1,5 +1,21 @@
 package month1_languageFoundation.Typecasting;
 
 public class ImplicitTypeCasting {
+	
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * implicit type casting
+		 * automatic conversion from smaller to bigger data Type
+		 * done by compiler automatically
+		 */
+		
+		int a=5;
+		long b=a;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
 
 }
